32ccb73752f1b572b8dbb072c8a8642810552e8a
Rudro Debnath <rudrorj88@gmail.com>
2025-09-10 22:07:09 +0600

    A. Vanya and Fence

---
+++ 677A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+int main()
+{
+    optimize();
+    int n, h, cnt = 0;
+    cin >> n >> h;
+    vector<int> a;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        a.push_back(x);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > h)
+            cnt += 2;
+        else
+            cnt++;
+    }
+    cout << cnt << endl;
+
+    return 0;
+}
