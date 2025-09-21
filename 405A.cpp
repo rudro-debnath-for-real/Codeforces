432d2f96c56aa2fdc6a154ff80b9b955339d623a
Rudro Debnath <rudrorj88@gmail.com>
2025-09-21 17:17:12 +0600

    A. Gravity Flip

---
+++ 405A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+int main()
+{
+    optimize();
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> vec[i];
+    }
+    sort(vec.begin(), vec.end());
+    for (int i = 0; i < n; i++)
+    {
+        cout << vec[i] << " ";
+    }
+    return 0;
+}
