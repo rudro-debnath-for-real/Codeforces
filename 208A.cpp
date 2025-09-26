2204507d7cf88c6cfee8c3c85f946cee39e8a18a
Rudro Debnath <rudrorj88@gmail.com>
2025-09-26 22:38:46 +0600

    A. Dubstep

---
+++ 208A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+int main()
+{
+    optimize();
+    string s, k;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B')
+        {
+            i += 2;
+            k.push_back(' ');
+        }
+        else
+        {
+            k.push_back(s[i]);
+        }
+    }
+    cout << k << endl;
+
+    return 0;
+}
