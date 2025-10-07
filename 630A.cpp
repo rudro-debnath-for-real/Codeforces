91656a39bd78e5dd7a29b0de49b1b3373e5c6d15
Rudro Debnath <rudrorj88@gmail.com>
2025-10-07 08:04:40 +0600

    A. Again Twenty Five!

---
+++ 630A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+int main()
+{
+    optimize();
+
+    int n;
+    cin >> n;
+    cout << "25";
+    return 0;
+}
