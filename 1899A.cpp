0000328b42ba4a57471cca10c2ad2ff4ae20ce61
Rudro Debnath <rudrorj88@gmail.com>
2025-10-11 08:28:06 +0600

    A. Game with Integers

---
+++ 1899A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+int main()
+{
+    optimize();
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        int a;
+        cin >> a;
+        if (a % 3 == 0)
+            cout << "Second" << endl;
+        else
+            cout << "First" << endl;
+    }
+
+    return 0;
+}
