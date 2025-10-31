754a1083e4b9e1d4361c6cf53ef034df052236dc
Rudro Debnath <rudrorj88@gmail.com>
2025-10-31 07:55:05 +0600

    D. Yet Another Array Problem

---
+++ 2167D.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- Fast I/O ----------
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// ---------- Shortcuts ----------
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define endl '\n'
+
+// ---------- Constants ----------
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+// ---------- Debug (optional) ----------
+#define debug(x) cerr << #x << " = " << x << endl;
+
+// ---------- Direction Arrays (for grid problems) ----------
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+/*
+
+*/
+
+// ---------- Main Function ----------
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> vec(n);
+    for (auto &it : vec)
+        cin >> it;
+    bool g = false;
+    for (ll i = 2; i <= 100; i++)
+    {
+        for (ll j = 0; j < (ll)vec.size(); j++)
+        {
+            if (__gcd(i, vec[j]) == 1)
+            {
+                cout << i << endl;
+                return;
+            }
+        }
+    }
+}
+
+int main()
+{
+    optimize();
+    int t = 1;
+    // Uncomment next line for multiple test cases
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
