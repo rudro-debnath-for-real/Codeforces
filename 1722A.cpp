496f750e8679cba05aa62f15d415844cb283ad1c
Rudro Debnath <rudrorj88@gmail.com>
2025-11-01 21:10:09 +0600

    A: Spell Check

---
+++ 1722A.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- Fast I/O ----------
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// ---------- Shortcuts ----------
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define endl '\n'
+
+// ---------- Constants ----------
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+// ---------- Debug (optional) ----------
+#define debug(x) cerr << #x << " = " << x << endl;
+
+// ---------- Direction Arrays (for grid problems) ----------
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+
+/*
+    observaton:
+
+*/
+
+// ---------- Main Function ----------
+void solve()
+{
+    int n;
+    cin >> n;
+    string str_MAIN = "Timur";
+    sort(str_MAIN.begin(), str_MAIN.end());
+    string str_IN;
+    cin >> str_IN;
+    sort(str_IN.begin(), str_IN.end());
+    if (n == 5)
+    {
+        if (str_MAIN == str_IN)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    optimize();
+    int t = 1;
+    // Uncomment next line for multiple test cases
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
